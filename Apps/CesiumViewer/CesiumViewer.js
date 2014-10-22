7388cdfdf46f0fb43afe1253ccbfbf52a6123584
Andreas Silva <andreas.silva@knowledgeworks.pt>
2014-10-22 17:31:15 +0100

    latest cesium lib

--- Apps/CesiumViewer/CesiumViewer.js
+++ Apps/CesiumViewer/CesiumViewer.js
@@ -5,6 +5,7 @@ define([
         'Core/getFilenameFromUri',
         'DataSources/CzmlDataSource',
         'DataSources/GeoJsonDataSource',
+        'DataSources/KmlDataSource',
         'Scene/TileMapServiceImageryProvider',
         'Widgets/Viewer/Viewer',
         'Widgets/Viewer/viewerCesiumInspectorMixin',
@@ -17,6 +18,7 @@ define([
         getFilenameFromUri,
         CzmlDataSource,
         GeoJsonDataSource,
+        KmlDataSource,
         TileMapServiceImageryProvider,
         Viewer,
         viewerCesiumInspectorMixin,
@@ -108,6 +110,9 @@ define([
             loadPromise = dataSource.loadUrl(source);
         } else if (/\.geojson$/i.test(source) || /\.json$/i.test(source) || /\.topojson$/i.test(source)) {
             dataSource = new GeoJsonDataSource(getFilenameFromUri(source));
+            loadPromise = dataSource.loadUrl(source);
+        } else if (/\.kml$/i.test(source) || /\.kmz$/i.test(source)) {
+            dataSource = new KmlDataSource();
             loadPromise = dataSource.loadUrl(source);
         } else {
             showLoadError(source, 'Unknown format.');
